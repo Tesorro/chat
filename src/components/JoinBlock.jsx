604583ec203cd66d47bbabdab6a21c8921ddfde5
Tesorro <akarlssin@mail.ru>
2022-07-07 22:47:52 +0300

    project is done

--- src/components/JoinBlock.jsx
+++ src/components/JoinBlock.jsx
@@ -1,28 +1,43 @@
-import React from 'react'
-import socket from '../socket';
+import React from 'react';
+import axios from 'axios';
 
-function JoinBlock() {
+function JoinBlock({ onLogin }) {
+  const [roomId, setRoomId] = React.useState('');
+  const [userName, setUserName] = React.useState('');
+  const [isLoading, setLoading] = React.useState(false);
+
+  const onEnter = async () => {
+    if (!roomId || !userName) {
+      return alert('Неверные данные');
+    }
+    const obj = {
+      roomId,
+      userName,
+    };
+    setLoading(true);
+    await axios.post('/rooms', obj);
+    onLogin(obj);
+  };
+
   return (
-    <div className="input__roomid">
-      <div className="input-field col s6">
-        <input
-          placeholder="RoomID"
-          id="room_id"
-          type="text"
-          className="validate"
-        />
-      </div>
-      <div className="input-field col s6">
-        <input
-          placeholder="Name"
-          id="username"
-          type="text"
-          className="validate"
-        />
-      </div>
-      <a className="waves-effect waves-light btn">button</a>
+    <div className="join-block">
+      <input
+        type="text"
+        placeholder="Room ID"
+        value={roomId}
+        onChange={(e) => setRoomId(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Ваше имя"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button disabled={isLoading} onClick={onEnter} className="btn btn-success">
+        {isLoading ? 'ВХОД...' : 'ВОЙТИ'}
+      </button>
     </div>
   );
 }
 
-export default JoinBlock
+export default JoinBlock;
